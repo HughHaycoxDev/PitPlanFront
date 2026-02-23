3e9d6bea54af1f175472520e802d0aa4360d0124
b <belleguicjulian@gmail.com>
2026-02-23 14:44:23 +0100

    add driver

--- src/app/utilities/services/driver-roster.service.ts
+++ src/app/utilities/services/driver-roster.service.ts
@@ -20,6 +20,15 @@ export class DriverRosterService {
     return this.http.get<DriverRoster[]>(this.api + "list-by-race-plan/" + race_plan_id, {headers});
   }
 
+  createDriverOnDriverRoster(race_plan_id: number): Observable<DriverRoster> {
+    const token = this.auth.getToken();
+    const headers = token
+      ? new HttpHeaders({ Authorization: `Bearer ${token}` })
+      : new HttpHeaders();
+
+    return this.http.post<DriverRoster>(this.api + "create/driver/" + race_plan_id, {}, {headers});
+  }
+
   updateDriverOnDriverRoster(driver: DriverRoster): Observable<DriverRoster> {
     const token = this.auth.getToken();
     const headers = token
